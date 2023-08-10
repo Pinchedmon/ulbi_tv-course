0c5dc25fba0e235476b76fab11cb6a295dca31df
Alexey <temnikovalx@icloud.com>
2023-08-10 14:07:02 +0300

    added api.ts

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,28 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { User, userActions } from 'entities/User';
-import i18n from 'shared/config/i18n/i18n';
+import { ThunkConfig } from 'app/providers/StoreProvider';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 interface LoginByUsernameProps {
     username: string;
     password: string;
 }
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rejectValue: string}>(
+
+export const loginByUsername = createAsyncThunk<
+    User,
+    LoginByUsernameProps,
+    ThunkConfig<string>
+>(
     'login/loginByUsername',
-    async (authData, thunkAPI) => {
+    async (authData, thunkApi) => {
+        const { extra, dispatch, rejectWithValue } = thunkApi;
         try {
-            const response = await axios.post('http://localhost:8000/login', authData);
+            const response = await extra.api.post<User>('/login', authData);
 
             if (!response.data) {
                 throw new Error();
             }
+
             localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
+            dispatch(userActions.setAuthData(response.data));
             return response.data;
-        } catch (err) {
-            console.log(err);
-            return thunkAPI.rejectWithValue(i18n.t('Вы ввели неверный логин или пароль'));
+        } catch (e) {
+            console.log(e);
+            return rejectWithValue('error');
         }
     },
 );
